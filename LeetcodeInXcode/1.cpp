955d765f7745b429840e64674af2b0daeb6b0013
beddingearly <wangzitongnowb@163.com>
2018-08-17 16:56:13 +0800

    test

---
+++ LeetcodeInXcode/1.cpp
@@ -0,0 +1,9 @@
+//
+//  1.cpp
+//  LeetcodeInXcode
+//
+//  Created by bedding early on 2018/8/17.
+//  Copyright © 2018年 bedding early. All rights reserved.
+//
+
+#include "1.hpp"
---
+++ LeetcodeInXcode/1.hpp
@@ -0,0 +1,14 @@
+//
+//  1.hpp
+//  LeetcodeInXcode
+//
+//  Created by bedding early on 2018/8/17.
+//  Copyright © 2018年 bedding early. All rights reserved.
+//
+
+#ifndef __hpp
+#define __hpp
+
+#include <stdio.h>
+
+#endif /* __hpp */
